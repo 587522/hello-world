1d525d1c01ee38c3d78b1b919a479a17cdff8fb7
587522 <43036183+587522@users.noreply.github.com>
2018-09-07 09:33:10 -0400

    Update asst-0-setup.py

--- asst-0-setup.py
+++ asst-0-setup.py
@@ -1,6 +1,9 @@
 """
-primWrite a program in Python that calculates the area of a triangle.
+Write a program in Python that calculates the area of a triangle.
 Documentation: include a header and in-line comments
 """
 
-print("sdkhfklwjdhqfk")
+b = input("Enter a base: ")
+h = input("Enter a height: ")
+totalbh = int(b)*int(h)
+print(totalbh/2)
